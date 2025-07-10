4c64edd41ff290e495924197c726f3f1e91c9e53
shifa.patel <shifa.patel@pmechtech.com>
2025-07-10 15:10:55 +0530

    Add project files.

---
+++ App_1/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace App_1
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+
+        }
+       
+    }
+}
