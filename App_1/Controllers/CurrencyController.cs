d330474d7d650254dd38d38b97d12745026b70b7
shifa.patel <shifa.patel@pmechtech.com>
2025-07-11 18:06:09 +0530

    controller

--- App_1/Controllers/CurrencyController.cs
+++ App_1/Controllers/CurrencyController.cs
@@ -1,9 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace App_1.Controllers
+namespace App_1
 {
-    public class CurrencyController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class CurrencyController : ControllerBase
     {
-       
-    }
+        private readonly AppDbContext _context;
+        public CurrencyController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("/api/GetallCurrency")]
+        public IActionResult GetallCurrency()
+        {
+            var cur = _context.Currency.ToList();
+            return Ok(cur);
+        }
+    }   
 }
